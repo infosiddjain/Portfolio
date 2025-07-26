e8d5754fd27bf6c08799f56ef48f44499c3bba33
Siddharth <infosiddjain@gmail.com>
2025-07-26 10:34:22 +0530

    feat: skill page make it

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -16,13 +16,12 @@ export default function Home() {
   return (
     <MainLayout>
       <div className="flex flex-col-reverse md:flex-row items-center justify-center min-h-screen px-6 md:px-20">
-        {/* Left content */}
         <div className="w-full md:w-1/2 text-center md:text-left space-y-6">
           <div>
-            <h1 className="text-3xl md:text-5xl font-bold text-gray-800">
+            <h1 className="text-3xl md:text-5xl font-bold text-gray-800 font-montaga">
               Software Engineer
             </h1>
-            <p className="mt-4 text-gray-600 text-sm md:text-base leading-relaxed">
+            <p className="text-gray-600 text-sm md:text-base leading-relaxed font-poppins">
               Full Stack Developer and current Software Engineer with over 3+
               years of hands-on experience building scalable web and mobile
               applications using React.js, React Native, Node.js, and MongoDB.
@@ -35,8 +34,6 @@ export default function Home() {
               environments.
             </p>
           </div>
-
-          {/* Social Icons */}
           <div className="flex justify-center md:justify-start gap-4 mt-4 text-gray-600 text-xl">
             <FaFacebookF className="hover:text-blue-600 cursor-pointer" />
             <FaInstagram className="hover:text-pink-500 cursor-pointer" />
@@ -46,8 +43,6 @@ export default function Home() {
             <FaEnvelope className="hover:text-red-600 cursor-pointer" />
           </div>
         </div>
-
-        {/* Right image */}
         <div className="w-full md:w-1/2 flex justify-center mb-10 md:mb-0">
           <Image src={Logo} alt="logo" className="object-contain" priority />
         </div>
